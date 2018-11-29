4cc4ea029f2eec17d1f8605c74b2b89e69a4cb33
broodroosterdev <myth.usa538@gmail.com>
2018-11-29 23:57:18 +0100

    Added changes

--- ESP8266/src/Station.ino
+++ ESP8266/src/Station.ino
@@ -1,5 +1,44 @@
-#include "bsec.h";
+#include <bsec.h>
+#include <ESP8266WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include "./Wifi.h>
 
+/************************* WiFi Access Point *********************************/
+
+
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
+#define AIO_KEY         "...your AIO key..."
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
 
 // Helper functions declarations
 void checkIaqSensorStatus(void);
---
+++ ESP8266/src/Wifi.h
@@ -0,0 +1,6 @@
+#ifndef _WIFI_H_
+#define _WIFI_H_
+
+//Wifi Credentials needed for connecting to Access Point
+#define SSID "Basis station"
+#define PSK "09183351"
