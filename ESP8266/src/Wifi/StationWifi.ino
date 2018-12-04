9f982e42412db7af6d2d785a71200b75c75681f9
broodroosterdev <myth.usa538@gmail.com>
2018-12-04 19:40:39 +0100

    Added wifi and mqtt functionality

--- ESP8266/src/Wifi/StationWifi.ino
+++ ESP8266/src/Wifi/StationWifi.ino
@@ -2,41 +2,35 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
-#include "./WifiCreds.h"
+
+// Set the MQTT feeds to be used
+#define temperature_feed "/sensors/temperature"
+#define humidity_feed "/sensors/humidity"
+#define pressure_feed "/sensors/pressure"
+#define iaq_feed "/sensors/iaq"
 
 /************************* WiFi Access Point *********************************/
 
+WiFiClientSecure client;
+#define WLAN_SSID ""
+#define WLAN_PASS ""
 
+/************************* MQTT Broker Setup *********************************/
 
-/************************* Adafruit.io Setup *********************************/
-
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
-#define AIO_KEY         "...your AIO key..."
-
-/************ Global State (you don't need to change this!) ******************/
-
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
-// or... use WiFiFlientSecure for SSL
-//WiFiClientSecure client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+#define AIO_SERVER      "192.168.178.33"
+#define AIO_SERVERPORT  443                   // 8883 for MQTTS
+#define AIO_USERNAME    "mqttusername"
+#define AIO_KEY         "mqttpassword"
 
 /****************************** Feeds ***************************************/
 
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/photocell");
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT);
 
-// Setup a feed called 'onoff' for subscribing to changes.
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
-
+Adafruit_MQTT_Publish temperature_topic = Adafruit_MQTT_Publish(&mqtt, temperature_feed);
+Adafruit_MQTT_Publish pressure_topic = Adafruit_MQTT_Publish(&mqtt, pressure_feed);
+Adafruit_MQTT_Publish humidity_topic = Adafruit_MQTT_Publish(&mqtt, humidity_feed);
+Adafruit_MQTT_Publish iaq_topic = Adafruit_MQTT_Publish(&mqtt, iaq_feed);
 /*************************** Sketch Code ************************************/
-
-void MQTT_connect();
 
 // Helper functions declarations
 void checkIaqSensorStatus(void);
@@ -51,7 +45,20 @@ String output;
 void setup(void)
 {
   Serial.begin(115200);
+  int wifitry = 0;
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  delay(500);
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    wifitry++;
+    if (wifitry >= 50) {
+      Serial.println("Can't connect to WiFi, activating deepsleep");
+      wifitry = 0;
+    }
+  }
+  MQTT_connect();
   initBME680();
 }
 
@@ -63,11 +70,36 @@ void loop(void)
     output += ", " + String(iaqSensor.iaqEstimate);
     output += ", " + String(iaqSensor.iaqAccuracy);
     Serial.println(output);
+    temperature_topic.publish(iaqSensor.temperature);
+    humidity_topic.publish(iaqSensor.humidity);
+    pressure_topic.publish(iaqSensor.pressure);
+    iaq_topic.publish(iaqSensor.iaqEstimate);
+    Serial.println("Data posted to MQTT");
   } else {
     checkIaqSensorStatus();
   }
 }
 
+void MQTT_connect() {
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 5;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 1 second...");
+    mqtt.disconnect();
+    delay(500);  // wait a half second
+    retries--;
+    if (retries == 0) {
+    }
+  }
+  
+  Serial.println("MQTT Connected!");
+}
 // Helper function definitions
 void checkIaqSensorStatus(void)
 {
